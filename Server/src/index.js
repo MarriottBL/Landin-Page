97979bc0381bc3da583a5899855702cc14adc544
MarriottBL <brandonlmarriott@gmail.com>
2024-10-23 16:17:46 -0700

    added the path to the server/index.js

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();// Import environment variables from.env file
 
 const app = express();

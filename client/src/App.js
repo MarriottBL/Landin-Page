b2ed001b44256451138be334d1b80035b4d1da38
MarriottBL <brandonlmarriott@gmail.com>
2024-10-29 20:50:55 -0700

    update the build folder and the clients

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import Admin from "./Admin/admin";
 import './App.css';
 import ProfilePage from "./Components/HomePage/Main/mainPage";
-import Admin from "./Admin/admin";
 
 
 console.log("backend API URL:", process.env.REACT_APP_API_URL);
@@ -16,7 +16,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<ProfilePage />} />
-        <Route  path="/admin" element={<Admin />} />
+        {/* <Route  path="/admin" element={<Admin />} /> */}
       </Routes>
     </Router>
   </div>

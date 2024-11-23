1e840a6606afe65f91b279f8bdd83f51a49c3192
Marriott <marriott.brandon08@gmail.com>
2024-11-22 19:36:12 -0800

    Prepare for GitHub Pages deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import Admin from "./Admin/admin";
 import './App.css';
 import ProfilePage from "./Components/HomePage/Main/mainPage";
